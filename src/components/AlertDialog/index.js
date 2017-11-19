194577674903d2032ac3e80bc79cf1b15171efb1
wangyang <768188667@qq.com>
2017-11-19 18:29:50 +0800

    增加

--- src/components/AlertDialog/index.js
+++ src/components/AlertDialog/index.js
@@ -7,15 +7,23 @@ import Dialog, {
   DialogTitle,
 } from 'material-ui/Dialog';
 import Slide from 'material-ui/transitions/Slide';
+import { withStyles } from 'material-ui/styles';
+
+import {THEMBG} from '../../util/materialColor';
+
+const styles = theme => ({
+  label: {
+    color: THEMBG
+  }
+});
 
 function Transition(props) {
   return <Slide direction="up" timeout={{enter:100, exit:100}} {...props} />;
 }
 
 class AlertDialog extends React.Component {
-
   render() {
-    const {alertDialogData,handleRequestClose,handleRequestDelect} = this.props;
+    const {classes,alertDialogData,handleRequestClose,handleRequestDelect} = this.props;
     return (
       <div>
         <Dialog open={alertDialogData.open} onRequestClose={handleRequestClose} transition={Transition}>
@@ -24,8 +32,12 @@ class AlertDialog extends React.Component {
             <DialogContentText>{alertDialogData.content}</DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleRequestClose} color="primary">取消</Button>
-            <Button onClick={handleRequestDelect} color="primary" autoFocus>确认</Button>
+            <Button onClick={handleRequestClose} classes={{
+              label : classes.label
+            }}>取消</Button>
+            <Button onClick={handleRequestDelect} classes={{
+              label : classes.label
+            }}>确认</Button>
           </DialogActions>
         </Dialog>
       </div>
@@ -33,4 +45,4 @@ class AlertDialog extends React.Component {
   }
 }
 
-export default AlertDialog;
+export default withStyles(styles)(AlertDialog);
